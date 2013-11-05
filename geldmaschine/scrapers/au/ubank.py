cea76087573c7d2e75ee60ae271377f39c8b3c9b
Sebastian Vetter <sebastian@roadside-developer.com>
2013-11-05 19:04:16 +1100

    Fix UBank scraper after website changes

--- geldmaschine/scrapers/au/ubank.py
+++ geldmaschine/scrapers/au/ubank.py
@@ -13,24 +13,20 @@ class UbankAccountScraper(BaseAccountScraper):
     def login(self):
         self.browser.visit('https://www.ubank.com.au')
         with self.browser.get_iframe('loginFrame') as self.iframe:
+            self.iframe.is_element_present_by_css(
+                'input[name=f_username]', wait_time=10)
             self.iframe.fill('f_username', os.getenv('UBANK_EMAIL'))
             self.iframe.fill('password', os.getenv('UBANK_PASSWORD'))
             self.iframe.find_by_css('input[name=Login]')[0].click()
 
             self.iframe.is_element_present_by_css(
-                'div.tabularBorderAcctSumm>div>table',
-                wait_time=10
-            )
+                'div.tabularBorderAcctSumm>div>table', wait_time=10)
 
     def scrape_account_details(self):
-        table_css = 'div.tabularBorderAcctSumm>div>table'
+        table_css = 'table[id$=db1]'
 
         # Make sure that content is fully loaded
-        self.iframe.is_element_not_present_by_css(
-            'span[class$=af_inputText_content]',
-            1  # second
-        )
-
+        self.iframe.is_element_not_present_by_css(table_css, 5)  # seconds
         for elem in self.iframe.find_by_css(table_css):
             soup = BeautifulSoup(elem.html)
             strings = [ss for ss in soup.stripped_strings]
@@ -42,4 +38,4 @@ class UbankAccountScraper(BaseAccountScraper):
             account.balance = D(strings[4].replace(',', '').replace('$', ''))
 
     def logout(self):
-        pass
+        self.iframe.find_by_css('div[class=ActionLogout]>a')[0].click()
